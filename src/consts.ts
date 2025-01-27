0dc6de7a9c3e6ca9827b1190b51d2db6942af234
flhcx <awws_@outlook.com>
2025-01-27 20:57:06 +0800

    2313

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Koibumi Blog';
+export const SITE_TITLE = 'QWQ';//博客标题
 
-export const SITE_DESCRIPTION = 'A blog about SolidJS, Astro, and koibumi design.';
+export const SITE_DESCRIPTION = '一个电脑小白的博客';
 
-export const FAVICON_SRC = '/favicon.svg';
+export const FAVICON_SRC = '/favicon.svg';//
 
-export const AVATAR_SRC = '/avatar.avif';
+export const AVATAR_SRC = '/avatar.avif';//头像位置
 
-export const BIO = "This is a demo site for Koibumi Astro Theme. Fork this from github and start your own blog!";
+export const BIO = "不会东西aaaa";//作者简介
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
 
-export const COPYRIGHT_NAME = 'Koibumi Design';
+export const COPYRIGHT_NAME = 'flhcx';
 
