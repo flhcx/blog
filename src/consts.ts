928b53e2f1442bef6c517a295ed4ea3d24ebec22
flhcx <awws_@outlook.com>
2025-01-28 10:29:04 +0800

    13

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Awws';//博客标题
+export const SITE_TITLE = 'Awws在嬉戏玩耍';//博客标题
 
 export const SITE_DESCRIPTION = '一个电脑小白的博客';
 
@@ -9,7 +9,7 @@ export const FAVICON_SRC = '/favicon.svg';//
 
 export const AVATAR_SRC = '/avatar.avif';//头像位置
 
-export const BIO = "不会东西aaaa";//作者简介
+export const BIO = "黑夜中的明光，便是心之所向";//作者简介
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
 
